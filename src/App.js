2fa10e08320c0f2cdaaa2f8826f694319bc606eb
MilesN29 <mdn29@pitt.edu>
2024-11-23 20:57:15 -0500

    fixing links

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
-import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Contact from './pages/Contact';
-import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 
 const App = () => {
   return (
@@ -9,6 +8,7 @@ const App = () => {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/contact" element={<Contact />} />
+        <Route path="*" element={<Home />} /> {/* This will catch all unmatched routes */}
       </Routes>
     </Router>
   );
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,17 +1,17 @@
 import React from 'react';
-//import './Navbar.css';
+import { Link } from 'react-router-dom';
 
 const Navbar = () => {
-    return (
-        <div className="top_nav_bar">
-            <div className="nav_links">
-                <a href="/">Home</a>
-                <a href="/contact">Contact</a>
-                <a href="https://github.com/MilesN29" target="_blank" rel="noopener noreferrer">GitHub</a>
-                <a href="https://www.linkedin.com/in/miles-newland-34518a262/" target="_blank" rel="noopener noreferrer">LinkedIn</a>
-            </div>
-        </div>
-    );
+  return (
+    <div className="top_nav_bar">
+      <div className="nav_links">
+        <Link to="/">Home</Link>
+        <Link to="/contact">Contact</Link>
+        <a href="https://github.com/MilesN29" target="_blank" rel="noopener noreferrer">GitHub</a>
+        <a href="https://www.linkedin.com/in/miles-newland-34518a262/" target="_blank" rel="noopener noreferrer">LinkedIn</a>
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
